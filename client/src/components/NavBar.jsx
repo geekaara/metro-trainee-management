a18e11efe380d983823a51962366338ff216ac96
s3955464 <s3955464@student.rmit.edu.au>
2024-05-11 20:41:36 +1000

    Fixed NavBar issue

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -2,10 +2,10 @@ import "../css/NavBar.css";
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import metroImage from "../images/Banner.png";
-import { Icon, Box} from "@chakra-ui/react";
-import { AiOutlineHome } from "react-icons/ai";
+import { Box, Icon } from "@material-ui/core";
 import { FaChevronUp, FaChevronDown } from "react-icons/fa";
 import { CgProfile } from "react-icons/cg";
+
 function Navbar(props) {
   const [showInstructorDropdown, setShowInstructorDropdown] = useState(false);
   const [showModuleDropdown, setShowModuleDropdown] = useState(false);
@@ -26,43 +26,47 @@ function Navbar(props) {
     setShowModuleDropdown(false); // Close the module dropdown when opening course dropdown
   };
 
-
-
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-green" >
+    <nav className="navbar navbar-expand-lg navbar-dark bg-green">
       <ul className="navbar-nav flex-column">
         <li className="nav-item">
           <Link className="navbar-brand" to="/">
-          <img
-                src={metroImage} // Image source
-                style={{ 
-                    width: "250px", // Set width to 200px
-                    height: "auto", // Auto adjust height to maintain aspect ratio
-                    borderRadius: "5%", // Circular shape
-                    
-                }}
+            <img
+              src={metroImage} // Image source
+              style={{
+                width: "250px", // Set width to 200px
+                height: "auto", // Auto adjust height to maintain aspect ratio
+                borderRadius: "5%", // Circular shape
+              }}
             />
 
-           
-           <Box
-            bg="#0073CF" // Background color
-            borderRadius="40%" // Rounded border
-            p={4} // Padding
-            mr={4} // Margin right
-            color="white" // Text color
-            display="flex" // Flex display for alignment
-            alignItems="center" // Center align vertically
-            justifyContent="center" // Center align horizontally
-            width={40} // Width of the box
-            height={100} // Height of the box
-            marginLeft={50}
-        >
-            <CgProfile size={60} /> {/* Profile icon */}
-        </Box>
-        <h1 className="Profile-heading" style={{ color: "white", fontSize:"20px", marginLeft: "100px"}}>
-            Admin
+            <Box
+              bgcolor="white" // Background color
+              borderRadius="40%" // Rounded border
+              p={4} // Padding
+              mr={7} // Margin right
+              color="white" // Text color
+              display="flex" // Flex display for alignment
+              alignItems="center" // Center align vertically
+              justifyContent="center" // Center align horizontally
+              width={100} // Width of the box
+              height={100} // Height of the box
+               marginLeft={90}
+              marginRight={100}
+             
+            >
+              <CgProfile style={{
+                width: "60px", // Set width to 200px
+                height: "auto", // Auto adjust height to maintain aspect ratio
+                borderRadius: "5%", // Circular shape
+                marginLeft: "90px",
+              }}/>
+               {/* Profile icon */}
+            </Box>
+            <h1 className="Profile-heading" style={{ color: "white", fontSize: "20px", marginLeft: "95px" }}>
+              Admin
             </h1>
-       </Link>
+          </Link>
         </li>
 
         <li className="nav-item">
@@ -104,29 +108,6 @@ function Navbar(props) {
             </ul>
           )}
         </li>
-{/* 
-        <li className="nav-item">
-          <span className="nav-link" onClick={toggleModuleDropdown}>
-            <div style={{ display: "flex", alignItems: "center" }}>
-              Modules
-              <Icon
-                as={showModuleDropdown ? FaChevronUp : FaChevronDown}
-                style={{ marginLeft: "90px" }}
-              />
-            </div>
-          </span>
-
-          {showModuleDropdown && (
-            <ul className="navbar-nav flex-column">
-              <li className="nav-item">
-                <Link className="nav-link" to="/add-modules">
-                  <span style={{ marginRight: "15px" }}>Add New Module</span>
-                </Link>
-              </li>
-    
-            </ul>
-          )}
-        </li> */}
 
         <li className="nav-item">
           <span className="nav-link" onClick={toggleCourseDropdown}>
